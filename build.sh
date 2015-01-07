618de5b257556e0520ce58b6edf5a06313bfac7d
8l <plan9assembler@gmail.com>
2015-01-08 05:55:44 +0900

    Update build.sh
    replace nasm with yasm

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nasm src/bootsectors/bmfs_mbr.asm -o bmfs_mbr.sys
-nasm src/bootsectors/pxestart.asm -o pxestart.sys
+yasm src/bootsectors/bmfs_mbr.asm -o bmfs_mbr.sys
+yasm src/bootsectors/pxestart.asm -o pxestart.sys
 cd src
-nasm pure64.asm -o ../pure64.sys
+yasm pure64.asm -o ../pure64.sys
 cd ..
